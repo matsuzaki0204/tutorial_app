a71547fd4adb4f4de697f09c59275ca3222343b6
matsuzaki0204 <matuzaki0204yusuke@gmail.com>
2019-04-06 15:31:47 +0000

    Finish static page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
-    root 'appleication#hello'
+    
+    root 'static_pages#home'
     
 
 end
