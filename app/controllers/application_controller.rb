4da1cc6090e28de944c0af8762b4b2b55303c47a
matsuzaki0204 <matuzaki0204yusuke@gmail.com>
2019-04-08 14:46:04 +0000

    Finish layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   
-<!DOCTYPE html>
-<html>
-    <head>
-        <title><%= full_title(yield(:title) %> | 勤怠管理システム</title>
-        <%= csrf_meta_tags %>
-        <%= stylesheet_link_tag  "application", media: "all", "data-turbolinks-track": "reload" %>
-        <%= javascript_include_tag "application", "data-turbolinks-track": "reload" %>
-    </head>
-   
-    <body>
-        <%= yield %>
-    </body>
-</html>
+  protect_from_forgery with: :exception
 
   def hello
     render html: "Hello, World!"
