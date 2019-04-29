0baee7889a7cb15309c50476a94cefc00fe843df
matsuzaki0204 <matuzaki0204yusuke@gmail.com>
2019-04-29 06:04:16 +0000

    Finish user edit index destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,15 @@ module SessionsHelper
         session.delete(:user_id)
         @current_user = nil
     end
-        
     
+    # 記憶しているURL（もしくはデフォルト値）にリダイレクトする
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+    
+    # アクセスしようとしたURLを記憶しておく
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
 end
